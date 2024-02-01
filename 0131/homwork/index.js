f0c12526122b8d389b6ba202a249826739506e23
Subash Karki <subashkarki68@gmail.com>
2024-02-01 15:24:24 +0545

    mern

---
+++ 0131/homwork/index.js
@@ -0,0 +1,13 @@
+//find out how to delete item inside array
+
+//Array sory method and sort the data in ascending order
+
+const group = [
+  { name: "Raktim", age: 30 },
+  { name: "Ruchi Raj", age: 26 },
+  { name: "Samundra", age: 26 },
+  { name: "Sumit", age: 19 },
+];
+
+group.pop();
+console.log(group);
