9a2b27a85f6ed29650481fde02c34efa50077252
Subash Karki <subashkarki68@gmail.com>
2024-02-06 18:49:25 +0545

    node basics

---
+++ 0206-nodejs/index.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const port = 8000;
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        data: "Hello World!",
+      })
+    );
+  })
+  .listen(port);
+
+console.log(`Application is running on port: ${port}`);
