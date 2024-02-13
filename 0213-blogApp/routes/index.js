4707d44f1b520a4f58bc0604060ae757da2682c0
Subash Karki <subashkarki68@gmail.com>
2024-02-13 23:22:55 +0545

    add route.rest

--- 0213-blogApp/routes/index.js
+++ 0213-blogApp/routes/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const blogRouter = require("../modules/blogs/blog.route");
 
-router.use("/blogs", blogRouter);
+router.use("/api/v1/blogs", blogRouter);
 
 module.exports = router;
